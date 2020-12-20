0af7f1d47f02ab21126b1428d596729b6d341066
tnsska2 <vnemftnsska2@naver.com>
2020-12-20 17:47:50 +0900

    a 태그 제거 후 script 코드로 변경

--- js/scroll.js
+++ js/scroll.js
@@ -1,14 +1,53 @@
-const subMenuList = document.querySelectorAll('.container .sideMenu ul li a');
+// Variable
+const subMenuList = document.querySelectorAll('.container .sideMenu ul li');
 const sectionList = document.querySelectorAll('.container .main section');
 
+// class
+const sectionManager = {
+    nav: document.getElementById('sectionNav'),
+    boxs: document.querySelectorAll('.main section'),
+    yList: null,
+    setSectionList() {
+       const yList = {};
+       [...this.nav.children].forEach(li => {
+           [...this.boxs].forEach(box => {
+               if (li.dataset.section === box.dataset.section) {
+                   yList[li.dataset.section] = box.offsetTop;
+               }
+           });
+       });
+
+       this.yList = yList;
+       return function (id) {
+           return yList[id];
+       };
+    },
+};
+
+// Function
+const clickSection = (e) => {
+    if (e.target.tagName === 'LI') {
+        const section = e.target.dataset.section;
+        const y = getY(section);
+        window.scrollTo(0, y);
+    }
+};
+
+const changeSectionColor = (section) => {
+    [...sectionManager.nav.children].forEach(v => {
+        if (v.dataset.section === section) v.classList.add('current');
+        else v.classList.remove('current');
+    });
+};
+
+const getY = sectionManager.setSectionList();
+
+// Event
 window.addEventListener('scroll', () => {
     const fromTop = window.scrollY;
-    subMenuList.forEach(link => {
-        const section = document.querySelector(link.hash);
-        if (fromTop >= section.offsetTop && section.offsetTop + section.offsetHeight > fromTop) {
-            link.classList.add('current');
-        } else {
-            link.classList.remove('current');
-        }
-    });
+    for (const section in sectionManager.yList) {
+        if (fromTop >= sectionManager.yList[section]) changeSectionColor(section);
+    }
 });
+
+sectionManager.nav.addEventListener('click', (e) => clickSection(e));
